fc644106fea60e3278e89741ea8157663d0cde54
c4llv07e <kseandi@gmail.com>
2022-04-06 00:59:49 +0300

    fixed print reverse bug
    Signed-off-by: c4llv07e <kseandi@gmail.com>

--- src/lua_lib.c
+++ src/lua_lib.c
@@ -2,21 +2,25 @@
 #include "lua_lib.h"
 
 extern Int print(lua_State* state);
+extern Int println(lua_State* state);
 
 typedef enum functions
   {
     print_func = 0x0,
+    println_func,
     functions_len,
   } functions;
 
 const char* functions_names[functions_len] =
   {
     [print_func] = "print",
+    [println_func] = "println",
   };
 
 Int (*functions_pointers[functions_len])(lua_State* state) =
   {
     [print_func] = print,
+    [println_func] = println,
   };
 
 
@@ -30,11 +34,18 @@ print(lua_State* state)
 
   for (Int i = 0x0; i < arg_len; ++i)
     {
-      log = lua_tolstring(state, -1, null);
-      lua_pop(state, 1);
+      log = lua_tolstring(state, i-arg_len, null);
       fputs(log, stdout);
     }
+  lua_pop(state, arg_len);
   return 0x0;
+}
+
+Int
+println(lua_State* state)
+{
+  lua_pushstring(state, "\n");
+  print(state);
 }
 
 
