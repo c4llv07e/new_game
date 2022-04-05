761771dbd92b06d98c3244c0e14e8c1e29ba16ae
c4llv07e <kseandi@gmail.com>
2022-04-05 12:53:34 +0300

    added debug param
    Signed-off-by: c4llv07e <kseandi@gmail.com>

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+SOURCES=$(find src -name "*.c")
+LIBS=(SDL2)
+LIB_PREFIX=${LIB_PREFIX:-"-l"}
+CC=${CC:-tcc}
+DEBUG=${DEBUG:-true}
+DEBUG_PREFIX=${DEBUG_PREFIX:-"-g"}
+
+[[ $DEBUG = true ]] \
+    && DEBUG_PARAM=$DEBUG_PREFIX \
+        || DEBUG_PARAM=''
+
+LIB_PARAM=""
+for i in ${LIBS[@]};
+do
+    LIB_PARAM="$LIB_PARAM $LIB_PREFIX$i ";
+done;
+
+[[ -n "$SOURCES" ]] \
+    && $CC $DEBUG_PARAM $LIB_PARAM $SOURCES -o main \
+    && exit 0 \
+    || echo "error on build" \
+    && exit 1
