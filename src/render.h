3bbaf56b8cad8a1fa4d3b2363e52dac9b17f3989
c4llv07e <38111072+c4llv07e@users.noreply.github.com>
2022-04-05 18:14:24 +0000

    moved structures upper

--- src/render.h
+++ src/render.h
@@ -6,16 +6,6 @@
 #include "types.h"
 #include "define.h"
 
-extern Int render_init(None);
-extern Int render_terminate(None);
-extern Return render_window_create(const char* name, Int width, Int height);
-extern Int render_window_destroy(Return window);
-extern Return render_window_show(Return window);
-extern Return render_window_clear(Return window);
-extern Bool render_window_should_close(Return window);
-extern Return render_window_poll_events(Return window);
-extern Return render_line(Return window, Int x,Int y,Int x2,Int y2, Render_color color);
-
 typedef struct Render_color
 {
   Byte r, g, b, a;
@@ -33,5 +23,15 @@ typedef struct Render_window
   Bool window_should_close;
   Render_window_option* option;
 } Render_window;
+
+extern Int render_init(None);
+extern Int render_terminate(None);
+extern Return render_window_create(const char* name, Int width, Int height);
+extern Int render_window_destroy(Return window);
+extern Return render_window_show(Return window);
+extern Return render_window_clear(Return window);
+extern Bool render_window_should_close(Return window);
+extern Return render_window_poll_events(Return window);
+extern Return render_line(Return window, Int x,Int y,Int x2,Int y2, Render_color color);
 
 #endif /* GAME_RENDER_INC */
