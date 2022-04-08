2545d80bb5cbb3b084c11f69a155ca8a421cbdd7
c4llv07e <38111072+c4llv07e@users.noreply.github.com>
2022-04-08 12:41:42 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ LIB_PREFIX=${LIB_PREFIX:-"-l"}
 CC=${CC:-tcc}
 DEBUG=${DEBUG:-true}
 DEBUG_PREFIX=${DEBUG_PREFIX:-"-g"}
+AM_GITHUB_ACTION=${AM_GITHUB_ACTION:-false}
+FOR_GITHUB_ACTION=""
 
 [[ $DEBUG = true ]] \
     && DEBUG_PARAM=$DEBUG_PREFIX \
@@ -17,8 +19,10 @@ do
     LIB_PARAM="$LIB_PARAM $LIB_PREFIX$i ";
 done;
 
+[[ $AM_GITHUB_ACTION = true ]] && FOR_GITHUB_ACTION="$FOR_GITHUB_ACTION -I/usr/include/lua5.3"
+
 [[ -n "$SOURCES" ]] \
-    && $CC $DEBUG_PARAM $LIB_PARAM $SOURCES -o main \
+    && $CC $DEBUG_PARAM $LIB_PARAM $FOR_GITHUB_ACTION $SOURCES -o main \
     && exit 0 \
     || echo "error on build" \
     && exit 1
